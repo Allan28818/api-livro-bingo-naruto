f91cc43e54163ed80d6fb6ca3ded2d79dfb9027c
isPixel <09testfree@email.com>
2025-05-27 23:23:07 -0300

    feat: filtragem por query params adicionada para busca de ninjas

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,82 @@ app.use(express.json());
 const localstorage = new LocalStorage("./database/ninjas");
 
 // Buscar os ninjas foragidos
-app.get("/", (req, res) => {
-  const ninjas = localstorage.getItem("ninjas");
-  const parsedNinjas = JSON.parse(ninjas || "[]");
-  res.status(200).json({ ninjas: parsedNinjas });
+app.get("/", async (req, res) => {
+  const ninjas = await localstorage.getItem("ninjas");
+  const parsedNinjas: NinjaProps[] = await JSON.parse(ninjas || "[]");
+
+// ==============================================================
+// ==============================================================
+
+  /** MODO SIMPLES  - filtro unico */
+
+  // const { name } = req.query;
+
+  // const resultado = parsedNinjas.filter(ninja => ninja.name == name)
+  // res.status(200).json(resultado)
+
+//================================================================
+//================================================================
+
+  /** MODO DINAMICO - filtros dinamicos */
+
+  // Primeiro recebemos os parametros e tranformamos em 'constantes' -- desestruturação de objeto.
+  const { id, name, country, age, style } = req.query;
+
+  // Salvamos os filtros e seus respectivos valores em um array chamado filtros.
+  // obs: poderiamos usar Object.entries/Object.keys, mas assim a visualização dos filtros fica mais fácil (preguiçoso, mas eficaz).
+  const filtros: { chave: keyof NinjaProps; valor: any }[] = [
+    { chave: 'id', valor: id },
+    { chave: 'name', valor: name },
+    { chave: 'country', valor: country },
+    { chave: 'age', valor: age },
+    { chave: 'style', valor: style }
+  ];
+
+  //console.log(ninjas)
+
+  // Vamos salvar na constante resultado somente o que passar pelo filter
+  const resultado = parsedNinjas.filter((item) => {
+
+    // Primeiro filtramos os filtros válidos, ou seja, que têm valor definido e não vazio
+    const filtrosValidos = filtros.filter(f => f.valor !== undefined && f.valor !== "");
+
+    // Se não houver nenhum filtro válido, retorna o item direto (traz todos os ninjas)
+    if (filtrosValidos.length === 0) return true;
+
+    // percorremos o array de filtros válidos
+    for (let filtro of filtrosValidos) {
+
+      // o item filtro vai ter as duas propriedades, chave e valor - desestruturação de objeto.
+      const { chave, valor } = filtro;
+
+      // a constante campo recebe o valor do objeto em questão com base na chave. 
+      const campo = item[chave as keyof NinjaProps];
+
+      // Se o campo for do tipo string precisamos fazer uma validação mais 'fina'
+      // garantir compatibilidade (maiúsculas e minúsculas)
+      // O que talvez seria resolvido só com um '==' em vez de '===', mas vale a garantia rs
+      if (typeof campo === "string") {
+        if (!campo.toLowerCase().includes(String(valor).toLowerCase())) {
+          return false;
+        }
+      }
+
+      // por final comparamos 
+      else {
+        if (String(campo) !== String(valor)) {
+          return false;
+        }
+      }
+    }
+
+    // se passar por todas as validações acima, é show papai seu item existe!
+    return true;
+  });
+
+  res.status(200).json({ ninjas: resultado });
 });
+
 
 // Adicionar à lista
 app.post("/add/ninja", (req, res) => {
